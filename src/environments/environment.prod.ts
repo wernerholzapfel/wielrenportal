2ce1d0ffd32e4c5f88b66057c9db1e862c10b0ee
wholzapfel <werner.holzapfel@trivento.nl>
2018-05-04 08:57:48 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://localhost:3000',
+  firebase: {
+    apiKey: 'AIzaSyDqZ6wBr5VZ2w3E0lt1fqaWdGeTLdsaHUM',
+    authDomain: 'wielerspel-aec4b.firebaseapp.com',
+    databaseURL: 'https://wielerspel-aec4b.firebaseio.com',
+    projectId: 'wielerspel-aec4b',
+    storageBucket: '',
+    messagingSenderId: '121086252622'
+  },
+  hmr: false
 };
