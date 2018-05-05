7e1f7330777bc2a53c5cdf0f733b49189f14bc2b
wholzapfel <werner.holzapfel@trivento.nl>
2018-05-05 10:49:20 +0200

    etappes toegevoegd aan admin

--- src/app/pipes/order-by/order-by.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OrderByPipe } from './order-by.pipe';
-
-xdescribe('OrderByPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OrderByPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
