51fdf3f42dc25af3abdd53e5816704abf22fd042
wholzapfel <werner.holzapfel@trivento.nl>
2018-05-18 10:17:16 +0200

    SSL

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 
+app.use(function (req, res, next) {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    console.log('https://' + req.header('host')+ req.url);
+    res.redirect('https://' + req.header('host') + req.url);
+  } else {
+    next();
+  }
+});
 // node redirect
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   console.log('redirect ');
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 
