bb2235f18618373151e977fb8b8aa020321f6703
wholzapfel <werner.holzapfel@trivento.nl>
2018-07-03 00:11:24 +0200

    spelregels iets aangepast/voorbereid tooltips toegevoegd

--- src/app/components/spelregels/spelregels.component.ts
+++ src/app/components/spelregels/spelregels.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GridOptions} from 'ag-grid';
 
 @Component({
   selector: 'app-spelregels',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spelregels.component.scss']
 })
 export class SpelregelsComponent implements OnInit {
-
+  spelregelstable: any[];
+  public gridOptions: GridOptions;
+  agColumns = [
+    {headerName: 'Positie', field: 'positie', minWidth: 80},
+    {headerName: 'Etappe', field: 'etappe', minWidth: 80},
+    {headerName: 'Algemeen', field: 'algemeen', minWidth: 80},
+    {headerName: 'Berg', field: 'berg', minWidth: 80},
+    {headerName: 'Punten', field: 'punten', minWidth: 80},
+    {headerName: 'Jongeren', field: 'jongeren', minWidth: 80},
+     ];
   constructor() { }
 
   ngOnInit() {
+    this.gridOptions = <GridOptions>{
+      columnDefs: this.agColumns,
+      onGridReady: () => {
+        this.gridOptions.api.sizeColumnsToFit();
+      },
+      enableSorting: true,
+    };
+
+    this.spelregelstable = [{
+        'positie': '1',
+        'etappe': 60,
+        'algemeen': 150,
+        'berg': 120,
+        'punten': 120,
+        'jongeren': 90
+    }];
   }
 
 }
