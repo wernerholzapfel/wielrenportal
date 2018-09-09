7cab5b46a56a72eb7140dfa968fd964011fa6977
wholzapfel <werner.holzapfel@trivento.nl>
2018-09-09 11:53:37 +0200

    laatste update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {IAppState} from '../../store/store';
-import {Store} from '@ngrx/store';
+import {select, Store} from '@ngrx/store';
 import {getLastUpdated, getNumberOne} from '../../store/participanttable/participanttable.reducer';
 import {Observable} from 'rxjs';
 import {IParticipanttable} from '../../models/participanttable.model';
 import {getTour, isRegistrationOpen} from '../../store/tour/tour.reducer';
 import {ITour} from '../../models/tour.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -16,16 +17,25 @@ import {ITour} from '../../models/tour.model';
 export class HomeComponent implements OnInit {
 
   lastUpdated$: Observable<any>;
+  lastUpdated: string;
   isRegistrationOpen$: Observable<boolean>;
   first$: Observable<IParticipanttable>;
   tour$: Observable<ITour>;
   deadline: Date;
 
   constructor(public authService: AuthService, public store: Store<IAppState>) {
+    moment.locale('nl');
   }
 
   ngOnInit() {
-    this.lastUpdated$ = this.store.select(getLastUpdated);
+
+
+    this.lastUpdated$ = this.store.pipe(select(getLastUpdated));
+    this.lastUpdated$.subscribe(lastupdated => {
+      if (lastupdated) {
+        this.lastUpdated = moment(lastupdated.lastUpdated).fromNow();
+      }
+    });
     this.isRegistrationOpen$ = this.store.select(isRegistrationOpen);
     this.first$ = this.store.select(getNumberOne);
 
