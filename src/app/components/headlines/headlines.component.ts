9ddf2fd996424849647deefbf7967e9976002578
wholzapfel <werner.holzapfel@trivento.nl>
2019-03-22 21:41:57 +0100

    headline bugje gefixed + top 5 fixje

--- src/app/components/headlines/headlines.component.ts
+++ src/app/components/headlines/headlines.component.ts
@@ -4,8 +4,8 @@ import {HeadlinesService} from '../../services/headlines.service';
 import {select, Store} from '@ngrx/store';
 import {IAppState} from '../../store/store';
 import {getTour} from '../../store/tour/tour.reducer';
-import {takeUntil} from 'rxjs/operators';
-import {Observable, Subject} from 'rxjs';
+import {switchMap, takeUntil} from 'rxjs/operators';
+import {Observable, of, Subject} from 'rxjs';
 import {ITour} from '../../models/tour.model';
 
 @Component({
@@ -29,24 +29,28 @@ export class HeadlinesComponent implements OnInit, OnDestroy {
 
     this.tour$ = this.store.pipe(select(getTour));
 
-    this.tour$.pipe(takeUntil(this.unsubscribe)).subscribe(tour => {
-      this.headlineService.getHeadlines(tour.id).subscribe(headlines => {
-          if (headlines) {
-            this.headlines = headlines;
-            this.headline = headlines[this.headlineIndex];
-          }
+    this.tour$.pipe(switchMap(tour => {
+      return tour ? of(tour.id) : of(null);
+    }))
+      .pipe(switchMap(tourId => {
+        return tourId ? this.headlineService.getHeadlines(tourId) : of(null);
+      }))
+      .pipe(takeUntil(this.unsubscribe)).subscribe(headlines => {
+        if (headlines) {
+          this.headlines = headlines;
+          this.headline = headlines[this.headlineIndex];
         }
-      );
-    });
+      }
+    );
   }
 
   nextHeadline() {
-    this.headlineIndex ++;
+    this.headlineIndex++;
     this.headline = this.headlines[this.headlineIndex];
   }
 
   previousHeadline() {
-    this.headlineIndex --;
+    this.headlineIndex--;
     this.headline = this.headlines[this.headlineIndex];
   }
 
