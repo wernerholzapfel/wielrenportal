eb707a69300e5a572aface4d5229f373918910cf
wholzapfel <werner.holzapfel@trivento.nl>
2019-03-22 21:35:11 +0100

    stand van deelnemer toegevoegd aan top5

--- src/app/components/top5/top5.component.ts
+++ src/app/components/top5/top5.component.ts
@@ -1,26 +1,48 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {getTopX} from '../../store/participanttable/participanttable.reducer';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {getParticipantPredictions, getTopX} from '../../store/participanttable/participanttable.reducer';
 import {Store} from '@ngrx/store';
 import {IAppState} from '../../store/store';
-import {Observable} from 'rxjs';
+import {Observable, of, Subject} from 'rxjs';
 import {IParticipanttable} from '../../models/participanttable.model';
+import {getParticipant} from '../../store/participant/participant.reducer';
+import {switchMap, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-top5',
   templateUrl: './top5.component.html',
   styleUrls: ['./top5.component.scss']
 })
-export class Top5Component implements OnInit {
-  top = 5;
+export class Top5Component implements OnInit, OnDestroy {
 
   constructor(public store: Store<IAppState>) {
   }
 
+  top = 5;
+  unsubscribe = new Subject<void>();
   @Input() lastUpdated: string;
   @Input() isRegistrationOpen$: Observable<boolean>;
   stand$: Observable<IParticipanttable[]>;
+  participantPrediction: IParticipanttable;
 
   ngOnInit() {
     this.stand$ = this.store.select(getTopX(this.top));
+
+    this.store.select(getParticipant)
+      .pipe(takeUntil(this.unsubscribe), switchMap(participant => {
+      return participant ? of(participant.id) : of(null);
+    }))
+      .pipe(switchMap(participantId => {
+        return participantId ? this.store.select(getParticipantPredictions(participantId)) : of(null);
+      }))
+      .subscribe(participantPredictions => {
+      if (participantPredictions) {
+        this.participantPrediction = participantPredictions;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 }
--- src/app/models/participanttable.model.ts
+++ src/app/models/participanttable.model.ts
@@ -5,5 +5,6 @@ export interface IParticipanttable {
   displayName: string;
   predictions: IPrediction[];
   totalPoints: number;
+  position?: number;
 }
 
