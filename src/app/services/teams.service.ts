7f53299d2575b0e90853f41bdc32a7f1ea2a560e
wernerholzapfel <werner.holzapfel@trivento.nl>
2020-08-20 22:51:34 +0200

    sortering teams in frontend aangepast

--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {ITeam} from '../models/team.model';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class TeamService {
@@ -11,6 +12,17 @@ export class TeamService {
   }
 
   getTeams(): Observable<ITeam[]> {
-    return this.http.get<ITeam[]>(`${environment.apiBaseUrl}/teams`);
+    return this.http.get<ITeam[]>(`${environment.apiBaseUrl}/teams`)
+      .pipe(map(response => {
+        return response.sort((a, b) => {
+          if (a.teamName > b.teamName) {
+            return 1;
+          }
+          if (a.teamName < b.teamName) {
+            return -1;
+          }
+          return 0;
+        });
+      }));
   }
 }
